2379819bf7fbe8ee512b4e0d55bfd55f6e881da6
mushaid01 <67502604+mushaid01@users.noreply.github.com>
2022-06-02 04:36:37 -0700

    Add files via upload

---
+++ flask_test/newsapp/config.py
@@ -0,0 +1,6 @@
+API_PREFIX               = '/api/v1'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI        = 'sqlite:///newsdata.db'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:3328@localhost:5432/flask'
+API_KEY="<1852edfa44654869bc943ccbbcd14c88>" #newsapp.org
+
